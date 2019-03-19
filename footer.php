9343b27f37a35c0bc4d8bb8d8fddb793fd6499d9
Saitou Yoshihisa <anti_viral_drug@yahoo.co.jp>
2019-03-20 08:09:49 +0900

    テンプレート作成

--- footer.php
+++ footer.php
@@ -1,15 +1,8 @@
-<?php
-//$page_data = get_page_by_path('bannerbox');
-//$page = get_post($page_data);
-//echo $page->post_content;
-?>
+
 </div>
 <footer>
-    <div class="box1">
-        <div class="logo">ヘッダー abc株式会社</div>
-        <address>abc株式会社<br>〒000-0000<br>東京都</address>
-    </div>
-    <nav><?php //wp_nav_menu(['theme_location'=>'footer-navi']); ?></nav>
+    <address>〒000-0000<br>東京都〇〇区△△1-1-1</address>
+    <nav><?php wp_nav_menu(['theme_location'=>'footer-navi']); ?></nav>
     <small>Copyright © 20xx abc Co.,Ltd. All Rights Reserved.</small>
 </footer>
 </div>
